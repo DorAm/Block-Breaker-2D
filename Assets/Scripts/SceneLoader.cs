eb94b1db2362c6de62d7b98343e59db8b41eebe6
Dor Amar <damar@paloaltonetworks.com>
2018-08-24 20:55:51 +0300

    wip

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private const int FIRST_SCENE_ID = 0;
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(FIRST_SCENE_ID);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
