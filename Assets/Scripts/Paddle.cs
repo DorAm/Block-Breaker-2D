cdec27f8d9a280168170be822f53efc18a510c8c
DorAm <bndor.am@gmail.com>
2018-08-25 01:04:44 +0300

    wip

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Constants;
+
+public class Paddle : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * Constants.Constants.HORIZONTAIL_UNITS;
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
+	}
+}
