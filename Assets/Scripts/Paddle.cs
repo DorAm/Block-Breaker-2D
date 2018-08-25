ab9895cd8b4470d26781da1c7cab2a9593751fa6
DorAm <bndor.am@gmail.com>
2018-08-25 17:30:21 +0300

    wip

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
 using Constants;
 
 public class Paddle : MonoBehaviour {
@@ -12,6 +14,7 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log(Input.mousePosition);
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * Constants.Constants.HORIZONTAIL_UNITS;
         Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
         transform.position = paddlePosition;
