32465cae725b561b2144bdcd52485f80df56f079
DorAm <bndor.am@gmail.com>
2018-08-25 17:35:25 +0300

    switched to rider

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,13 +7,8 @@ using Constants;
 
 public class Paddle : MonoBehaviour {
     
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		Debug.Log(Input.mousePosition);
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * Constants.Constants.HORIZONTAIL_UNITS;
         Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
