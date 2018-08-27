ba9d6df9cf7a69adeec92dc030204649c0ad4d0c
DorAm <bndor.am@gmail.com>
2018-08-27 17:07:39 +0300

    BB3 created level 1

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	// config params
+	[SerializeField] private Paddle paddle1;
+	[SerializeField] private float xPush = 2f;
+	[SerializeField] private float yPush = 15f;
+	
+	// state
+	private Vector2 paddleToBallVector;
+	private bool ballLaunched = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!ballLaunched)
+		{
+			LockBallToPaddle();
+		}
+		LaunchOnMouseClick();
+	}
+
+	private void LockBallToPaddle()
+	{
+		Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+		transform.position = paddlePosition + paddleToBallVector;
+	}
+
+	private void LaunchOnMouseClick()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			ballLaunched = true;
+			GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+		}	
+	}
+}
