6840a7a4f96ca7555c98f02e29e1005c6f5a5a71
ranb66 <djspikestyle@gmail.com>
2018-08-27 18:23:25 +0300

    wip

--- Assets/Scripts/close.cs
+++ Assets/Scripts/close.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SampleScript : MonoBehaviour {
+public class close : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,11 @@ public class SampleScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
 }
