4dc23f96f7099877b202adafb29b9986d3d66485
ranb66 <djspikestyle@gmail.com>
2018-08-27 23:19:50 +0300

    BB8 adds player prefs

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-public class Block : MonoBehaviour
-{
-    private Level level;
+public class Block : MonoBehaviour {
 
+    private Level level;
+    public PlayerPrefs1 playerPrefs1;
     private void Start()
     {
+        playerPrefs1 = FindObjectOfType<PlayerPrefs1>();
         level = FindObjectOfType<Level>();
         level.IncBreakableBlocksCount();
     }
@@ -16,6 +17,7 @@ public class Block : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        playerPrefs1.AddScore(50);
         level.BlockDestroyed();
     }
 }
---
+++ Assets/Scripts/PlayerReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        PlayerPrefs.SetInt("level", 0);
+        PlayerPrefs.SetInt("score", 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
